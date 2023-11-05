1e3e21519a13358ecdb9647502845cd0b5d6b724
kamilbeb <bebkampoczta@gmail.com>
2023-11-05 17:39:40 +0100

    Add get_user_avatar

--- twitter.py
+++ twitter.py
@@ -1,27 +1,47 @@
+import json
 import os.path
 import re
+from urllib.parse import urljoin
+
+import requests as requests
+
+USERS_API = "https://api.github.com/users"
 
 
 class Twitter:
     version = '1.0'
 
-    def __init__(self, backend=None):
+    def __init__(self, backend=None, username=None):
         self.backend = backend
         self._tweets = []  # atrybut prywatny
+        self.username = username
 
     @property
     def tweets(self):
         if self.backend and not self._tweets:
-            self._tweets = [line.rstrip('\n') for line in self.backend.readlines()]
+            backend_text = self.backend.read()
+            if backend_text:
+                self._tweets = json.loads(backend_text)
         return self._tweets
 
+    @property
+    def tweet_massages(self):
+        return [tweet['message'] for tweet in self.tweets]
+
+    def get_user_avatar(self):
+        if not self.username:
+            return None
+
+        url = urljoin(USERS_API, self.username)
+        resp = requests.get(url)
+        return resp.json()['avatar_url']
     def tweet(self, message):
         if len(message) > 160:  # jeżeli wiadomość ma więcej niż 160 znaków to wywołaj błąd
             raise Exception(
                 "Message too long")  # Exception podstawowa klasa wyjątków pierwszy parametr to jest komunikat
-        self.tweets.append(message)
+        self.tweets.append({'message': message, 'avatar': self.get_user_avatar()})
         if self.backend:
-            self.backend.write("\n".join(self.tweets))
+            self.backend.write(json.dumps(self.tweets))
 
     def find_hashtags(self, message):  # metoda wyszukująca w wiadomości hasztag
         return [m.lower() for m in re.findall("#(\w+)", message)]
--- twitter_test.py
+++ twitter_test.py
@@ -26,14 +26,14 @@ def test_twitter_initialization(twitter):
 
 def test_tweet_single_message(twitter):
     twitter.tweet('Test message')
-    assert twitter.tweets == ['Test message']
+    assert twitter.tweet_massages == ['Test message']
 
 
 def test_tweet_long_message(twitter):
     with pytest.raises(Exception):  # sprawdzamy wystąpienie wyjątku w momencie gdy tekst jest dłuższy niż 160 znaków
         twitter.tweet(
             'test' * 41)  # tutaj będziemy sprawdzać czy nasz błąd spowoduje wywołanie wyjątku. Pytest da wynik pozytywny bo spodziewaliśmy się tego błędu
-    assert twitter.tweets == []  # sprawdzenie że ten tweet nie został dodany do tweets
+    assert twitter.tweet_massages == []  # sprawdzenie że ten tweet nie został dodany do tweets
 
 def test_initialize_two_twitter_classes(backend):
     twitter1 = Twitter(backend=backend)
@@ -42,7 +42,7 @@ def test_initialize_two_twitter_classes(backend):
     twitter1.tweet('Test 1')
     twitter1.tweet('Test 2')
 
-    assert twitter2.tweets == ['Test 1', 'Test 2']
+    assert twitter2.tweet_massages == ['Test 1', 'Test 2']
 @pytest.mark.parametrize("message, expected", (
         ("Test #first message", ["first"]),
         ("#first Test message", ["first"]),
